521f2df12156e892d2055d17734de0379f2d1553
Areg Nakashian <ss4crifice@gmail.com>
2024-10-25 16:31:56 -0400

    Part 2 of initial commit

---
+++ Source/cpptest/cpptest.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "cpptest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, cpptest, "cpptest" );
+ 
---
+++ Source/cpptest/cpptestGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "cpptestGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AcpptestGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AcpptestGameMode();
+};
+
+
+
